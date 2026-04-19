61c8be6dcc2797c0c8b761447826e81a6cc8cb12
Marcus Vinicius Nascimento Ferreira <dev.mvinicius@gmail.com>
2026-04-19 12:01:22 -0300

    feat: implement deck and quiz pages, add deck repository and store

---
+++ lib/pages/deck/deck_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DeckPage extends StatelessWidget {
+  const DeckPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
