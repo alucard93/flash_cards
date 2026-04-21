cb1c431b6f1219d1773c48c9139a3f3ca2915c46
Marcus Vinicius Nascimento Ferreira <dev.mvinicius@gmail.com>
2026-04-21 10:41:29 -0300

    feat: add service locator initialization for DeckRepository in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,26 @@
 import 'package:flash_cards/model/decks/deck.model.dart';
 import 'package:flash_cards/pages/deck/deck_page.dart';
+import 'package:flash_cards/repositories/deck.repository.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:get_it/get_it.dart';
 
 Future<void> inithive() async {
   await Hive.initFlutter();
   Hive.registerAdapter(DeckAdapter());
 }
 
+void initServiceLocator() {
+    GetIt.I.registerSingletonAsync<DeckRepository>(() async {
+    final box = await Hive.openBox<List>("decks");
+    return DeckRepository(boxDecks: box);
+  });
+}
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await inithive();
+  initServiceLocator();
   runApp(const MyApp());
 }
 
