e113ea4f1ed611460002c37111a1db11a2f00832
Marcus Vinicius Nascimento Ferreira <dev.mvinicius@gmail.com>
2026-04-21 09:40:07 -0300

    feat: implement removeDeck method in DeckRepository for deleting decks

--- lib/repositories/deck.repository.dart
+++ lib/repositories/deck.repository.dart
@@ -18,4 +18,11 @@ class DeckRepository {
     await boxDecks.put('decks', decks);
     await Future.delayed(const Duration(seconds: 2));
   }
+
+  Future<void> removeDeck(String id) async {
+    final decks = boxDecks.get('decks', defaultValue: [])!;
+    decks.removeWhere((deck) => deck.id == id);
+    await boxDecks.put('decks', decks);
+    await Future.delayed(const Duration(seconds: 2));
+  }
 }
