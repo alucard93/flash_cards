836ed32627df8d6fbec5d1613e91997ac7fd9917
Marcus Vinicius Nascimento Ferreira <dev.mvinicius@gmail.com>
2026-04-21 16:37:24 -0300

    feat: refactor DeckPage to include DeckStore and improve layout structure

--- lib/pages/deck/deck_page.dart
+++ lib/pages/deck/deck_page.dart
@@ -1,7 +1,9 @@
+import 'package:flash_cards/stores/decks.store.dart';
 import 'package:flutter/material.dart';
 
 class DeckPage extends StatelessWidget {
-  const DeckPage({super.key});
+  final store = DeckStore();
+  DeckPage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -11,43 +13,40 @@ class DeckPage extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.black,
       ),
-      body: Expanded(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Center(
-                child: Image.asset(
-                  key: Key("image"),
-                  'assets/images/no_decks.png',
-                  height: 300,
-                  fit: BoxFit.contain,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Center(
+              child: Image.asset(
+                key: Key("image"),
+                'assets/images/no_decks.png',
+                height: 300,
+                fit: BoxFit.contain,
+              ),
+            ),
+            SizedBox(height: 16),
+
+            OutlinedButton(
+              key: Key("btnOutlineAdicionar"),
+              onPressed: () {
+                Navigator.pushNamed(context, '/new-deck');
+              },
+              style: OutlinedButton.styleFrom(
+                side: BorderSide(color: Color(0xFFE1E1E1), width: 1),
+                minimumSize: const Size(double.infinity, 60),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(4),
                 ),
               ),
-
-              SizedBox(height: 16),
-
-              OutlinedButton(
-                key: Key("btnOutlineAdicionar"),
-                onPressed: () {
-                  Navigator.pushNamed(context, '/new-deck');
-                },
-                style: OutlinedButton.styleFrom(
-                  side: BorderSide(color: Color(0xFFE1E1E1), width: 1),
-                  minimumSize: const Size(double.infinity, 60),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(4),
-                  ),
-                ),
-                child: const Text(
-                  'Adicionar Deck',
-                  style: TextStyle(color: Colors.black),
-                ),
+              child: const Text(
+                'Adicionar Deck',
+                style: TextStyle(color: Colors.black),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
       floatingActionButton: Padding(
