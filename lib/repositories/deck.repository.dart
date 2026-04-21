59853093738c8894dd9626d2b7ab958bec3898a1
Marcus Vinicius Nascimento Ferreira <dev.mvinicius@gmail.com>
2026-04-21 09:33:22 -0300

    feat: implement DeckRepository with methods to retrieve all decks

--- lib/repositories/deck.repository.dart
+++ lib/repositories/deck.repository.dart
@@ -1,3 +1,15 @@
+import 'package:flash_cards/model/decks/deck.model.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
 class DeckRepository {
-  
+  final Box<List> boxDecks;
+
+  DeckRepository(this.boxDecks);
+
+  Future<List<Deck>> getAllDecks() async {
+    final decks = boxDecks.get('decks', defaultValue: []);
+    await Future.delayed(const Duration(seconds: 1)); // Simulate delay
+    return List<Deck>.from(decks!);
+  }
+
 }
